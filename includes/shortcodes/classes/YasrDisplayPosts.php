330687c30f4818ed4410f13a47c3622c0cc7efe2
Dudo1985 <d.curvino@gmail.com>
2023-01-25 11:49:48 +0100

    done method content

--- includes/shortcodes/classes/YasrDisplayPosts.php
+++ includes/shortcodes/classes/YasrDisplayPosts.php
@@ -73,43 +73,56 @@ class YasrDisplayPosts extends YasrShortcode {
      */
     public function returnShortcode() {
         // The Query
-        $the_query = new WP_Query($this->query_args);
+        $the_query         = new WP_Query($this->query_args);
 
         $shortcode_content = '';
 
         // The Loop
         if ($the_query->have_posts() ) {
             while ($the_query->have_posts()) : $the_query->the_post();
-
                 //This is page id or post id
                 $post_id = get_the_ID();
-                $shortcode_content .= "<div>
-                                            <h3 class='yasr-entry-title'>
-                                                <a href=".esc_url(get_the_permalink())." rel='bookmark'>
-                                                    ".esc_html(get_post_field( 'post_title', $post_id, 'raw' ))."
-                                                </a>
-                                            </h3>
-                                            <div class='yasr-entry-meta'>
-                                                <a href='".esc_url(get_author_posts_url(get_the_author_meta('ID')))."' class='entry-author'>
-                                                  ".get_the_author().
-                                                "</a>
-                                                 <span class='tp-post-item-date'>".get_the_date()."</span>
-                                            </div> <!-- End .entry-meta -->
-                                            <div class='yasr-entry-content'>
-                                                ".get_the_excerpt()."    
-                                            </div>
-                                        </div>";
+                $shortcode_content .= $this->content($post_id);
             endwhile;
+
             /* Restore original Post Data */
             wp_reset_postdata();
 
             $shortcode_content .= $this->pagination($the_query);
-
             return $shortcode_content;
-
         } else {
             return esc_html__('No posts found', 'yet-another-stars-rating');
         }
+    }
+
+    /**
+     * Return the shortcode content
+     *
+     * @author Dario Curvino <@dudo>
+     *
+     * @since 3.2.1
+     *
+     * @param $post_id
+     *
+     * @return string
+     */
+    public function content($post_id) {
+        return "<div>
+                    <h3 class='yasr-entry-title'>
+                        <a href=".esc_url(get_the_permalink())." rel='bookmark'>
+                            ".esc_html(get_post_field( 'post_title', $post_id, 'raw' ))."
+                        </a>
+                    </h3>
+                    <div class='yasr-entry-meta'>
+                        <a href='".esc_url(get_author_posts_url(get_the_author_meta('ID')))."' class='entry-author'>
+                          ".get_the_author().
+                        "</a>
+                         <span class='tp-post-item-date'>".get_the_date()."</span>
+                    </div> <!-- End .entry-meta -->
+                    <div class='yasr-entry-content'>
+                        ".get_the_excerpt()."    
+                    </div>
+                </div>";
     }
 
     /**
