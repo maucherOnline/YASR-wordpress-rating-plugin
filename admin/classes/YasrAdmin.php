3fadd983696600ca3af97cf66de6c42aa3dad166
Dudo1985 <d.curvino@gmail.com>
2023-03-01 09:25:13 +0100

    removed old backward compatibility code

--- admin/classes/YasrAdmin.php
+++ admin/classes/YasrAdmin.php
@@ -293,31 +293,7 @@ class YasrAdmin {
         //do only in admin
         if (is_admin() && current_user_can('activate_plugins')) {
             global $wpdb;
-            $yasr_stored_options = get_option('yasr_general_options');
-
             if (YASR_VERSION_INSTALLED !== false) {
-                //In version 2.6.6 %overall_rating% pattern is replaced with %rating%
-                //Remove March 2023
-                if (version_compare(YASR_VERSION_INSTALLED, '2.6.6') === -1) {
-                    if(array_key_exists('text_before_overall', $yasr_stored_options)) {
-                        $yasr_stored_options['text_before_overall'] =
-                            str_replace('%overall_rating%', '%rating%', $yasr_stored_options['text_before_overall']);
-                    }
-                }
-
-                //In version 2.7.4 option "text_before_stars" is removed.
-                //if it was set to 0, be sure that text before overall is empty
-                //Remove May 2023
-                if (version_compare(YASR_VERSION_INSTALLED, '2.7.4') === -1) {
-                    if (array_key_exists('text_before_stars', $yasr_stored_options)) {
-                        if($yasr_stored_options['text_before_stars'] === 0) {
-                            $yasr_stored_options['text_before_overall']  = '';
-                        }
-                    }
-                }
-
-                update_option('yasr_general_options', $yasr_stored_options);
-
                 //In version 2.9.7 the column comment_id is added
                 //Remove Dec 2023
                 if (version_compare(YASR_VERSION_INSTALLED, '2.9.7') === -1) {
